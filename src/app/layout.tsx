a2e1f82ecb2875d6735ef1b1edcdd6be730b2f47
maiconlara <maiconluan.lara@yahoo.com.br>
2024-03-07 12:27:09 -0300

    feat: first template configuration

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import "./globals.css";
+import type {Metadata} from "next";
+import {Poppins, Plus_Jakarta_Sans} from "next/font/google";
+
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+    variable: "--font-poppins",
+});
+const jakarta = Plus_Jakarta_Sans({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+    variable: "--font-jakarta",
+});
+
+export const metadata: Metadata = {
+    title: "NextJS Template",
+    description: "Enjooooy!",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body
+                className={`${poppins.variable} ${jakarta.variable} bg-background relative flex min-h-screen w-full max-w-full flex-col items-center justify-center overflow-x-hidden font-sans`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,47 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: "var(--font-poppins)",
+                jakarta: "var(--font-jakarta)",
+            },
+            colors: {
+                steel: {
+                    50: "#f5f7fa",
+                    100: "#eaeef4",
+                    200: "#d0dae7",
+                    300: "#a7bbd2",
+                    400: "#7798b9",
+                    500: "#567ba1",
+                    600: "#426187",
+                    700: "#374f6d",
+                    800: "#30445c",
+                    900: "#2c3b4e",
+                    950: "#0d1117",
+                },
+                black: {
+                    50: "#777676",
+                    100: "#888787",
+                    200: "#666565",
+                    300: "#4d4c4b",
+                    400: "#343332",
+                    500: "#1b1a19",
+                    600: "#020101",
+                    700: "#08070b",
+                    800: "#060609",
+                    900: "#040406",
+                    950: "#030303",
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
